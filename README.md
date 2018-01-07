b96248ca52e807f4b2165902ade6640a2e51b31b
Sunil Saibol <sunil_saibol@yahoo.com>
2018-01-07 22:30:37 +0530

    added maven application

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # java8feature
 This application contains java features sample applications
+
+07-JAN-18 : created sample application
---
+++ src/main/java/com/sai/sample/java8feature/App.java
@@ -0,0 +1,14 @@
+package com.sai.sample.java8feature;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String name = "sunil"; 
+        System.out.println( "Hello ".concat(name));
+    }
+}
